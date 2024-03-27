b3cefec628c60d9a49901112c34d3b71c29cdc8a
jansiyaja <147611491+jansiyaja@users.noreply.github.com>
2024-03-27 08:59:52 +0530

    Update multer.js

--- middleware/multer.js
+++ middleware/multer.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const storage = multer.diskStorage({
   destination : (req,file,cb)=>{
-    cb(null,path.join(__dirname,'../public/assets/images/product/productimages'))
+    cb(null,path.join(__dirname,'../public/assets/images/product/productImages'))
   },
   filename : function(req,file,cb){
     const name = Date.now()+'-'+file.originalname;
